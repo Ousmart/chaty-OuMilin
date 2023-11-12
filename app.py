a05f9d7d1370b210f5b53048e29e9401e094948b
Ousmart <uthaikhanthamongkol@gmail.com>
2023-11-12 09:40:53 +0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('message')
+def handle_message(message):
+    print('Message: ', message)
+    socketio.emit('message', message)
+
+if __name__ == '__main__':
+    socketio.run(app, host="172.24.11.160")
